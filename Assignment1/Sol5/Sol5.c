1799820d840739f99efc3eeffa67603c5a789840
Genesis99003167 <75408033+Genesis99003167@users.noreply.github.com>
2021-01-10 19:03:21 +0530

    Add files via upload

---
+++ Assignment1/Sol5/Sol5.c
@@ -0,0 +1,34 @@
+#include "Sol5.h"
+
+int main()
+{
+    pid_t ret;
+    int status;
+    ret=fork();
+    if(ret< 0)
+    {
+        perror("fork");
+        exit(1);
+    }
+    if(ret == 0)
+    {
+        int compile;
+        compile = execl("/usr/bin/gcc","gcc","-g","sol1.c",NULL);  // Link and compile the file
+        if(compile == 0)
+        {
+            perror("execv");
+            exit(2);
+        }
+        exit(0);
+
+    }
+    else
+    {
+        waitpid(-1,&status,0); 
+        printf("Exit status of parent--child = %d\n",
+			WEXITSTATUS(status));
+		
+    }
+    return 0;
+
+}
---
+++ Assignment1/Sol5/Sol5.h
@@ -0,0 +1,10 @@
+#ifndef __SOL5_H__
+#define __SOL5_H__
+
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+
+#endif
